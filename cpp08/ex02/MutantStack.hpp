2a11dc53cea6ae8e024f987e903b328ef8cd07ed
Cralos9 <carlosc.rocha77@gmail.com>
2024-08-07 15:32:54 +0100

    Cpp

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -6,17 +6,37 @@
 /*   By: cacarval <cacarval@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/07 13:53:21 by cacarval          #+#    #+#             */
-/*   Updated: 2024/08/07 14:01:18 by cacarval         ###   ########.fr       */
+/*   Updated: 2024/08/07 15:31:43 by cacarval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MUTANTSTACK_HPP
 #define MUTANTSTACK_HPP
 
+#include <iostream>
+#include <stack>
+#include <iterator>
 
-class MutantStack
+template <typename T> class MutantStack : public std::stack<T>
 {
-
+	public:
+		MutantStack() : std::stack<T>() {}
+		~MutantStack(){}
+		MutantStack(MutantStack const &cpy) : std::stack<T>()
+		{
+			*this = cpy;
+			}
+		MutantStack &operator=(MutantStack const &cpy)
+		{
+			if (this != &cpy) 
+			{
+				std::stack<T>::operator =(cpy);
+			}
+			return *this;
+		}
+		typedef typename std::stack<T>::container_type::iterator iterator;
+		iterator begin(){return (std::stack<T>::c.begin());}
+		iterator end(){return (std::stack<T>::c.end());}
 };
 
 
